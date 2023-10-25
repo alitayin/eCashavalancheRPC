ed5e55f6c9fef9a1463b2fd2d3ad17f6cb337b8b
alitayin <105124378+alitayin@users.noreply.github.com>
2023-10-25 22:42:42 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,183 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = 3080;
+const rateLimit = require("express-rate-limit");
+
+// 服务器 A 的 RPC 服务的 URL 和认证信息
+const rpcURL = '';
+const rpcUser = '';
+const rpcPassword = '';
+
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+}));
+
+app.get('/getavalanchepeerinfo', async (req, res) => {
+  const proofid = req.query.proofid;
+
+  try {
+    const response = await axios.post(rpcURL, {
+      jsonrpc: '1.0',
+      id: 'curltest',
+      method: 'getavalanchepeerinfo',
+      params: [proofid]
+    }, {
+      auth: {
+        username: rpcUser,
+        password: rpcPassword
+      }
+    });
+
+    res.send(response.data.result);
+  } catch (error) {
+    handleRPCError(error, res);
+  }
+});
+
+app.get('/getavalancheproofs', async (req, res) => {
+  try {
+    const response = await axios.post(rpcURL, {
+      jsonrpc: '1.0',
+      id: 'curltest',
+      method: 'getavalancheproofs'
+    }, {
+      auth: {
+        username: rpcUser,
+        password: rpcPassword
+      }
+    });
+
+    res.send(response.data.result);
+  } catch (error) {
+    handleRPCError(error, res);
+  }
+});
+
+app.get('/decodeavalanchedelegation', async (req, res) => {
+  const delegation = req.query.delegation;
+
+  try {
+    const response = await axios.post(rpcURL, {
+      jsonrpc: '1.0',
+      id: 'curltest',
+      method: 'decodeavalanchedelegation',
+      params: [delegation]
+    }, {
+      auth: {
+        username: rpcUser,
+        password: rpcPassword
+      }
+    });
+
+    res.send(response.data.result);
+  } catch (error) {
+    handleRPCError(error, res);
+  }
+});
+
+app.get('/verifyavalanchedelegation', async (req, res) => {
+  const delegation = req.query.delegation;
+
+  try {
+    const response = await axios.post(rpcURL, {
+      jsonrpc: '1.0',
+      id: 'curltest',
+      method: 'verifyavalanchedelegation',
+      params: [delegation]
+    }, {
+      auth: {
+        username: rpcUser,
+        password: rpcPassword
+      }
+    });
+
+    res.send(response.data.result);
+  } catch (error) {
+    handleRPCError(error, res);
+  }
+});
+
+
+app.get('/decodeavalancheproof', async (req, res) => {
+  const proof = req.query.proof;
+
+  try {
+    const response = await axios.post(rpcURL, {
+      jsonrpc: '1.0',
+      id: 'curltest',
+      method: 'decodeavalancheproof',
+      params: [proof]
+    }, {
+      auth: {
+        username: rpcUser,
+        password: rpcPassword
+      }
+    });
+
+    res.send(response.data.result);
+  } catch (error) {
+    handleRPCError(error, res);
+  }
+});
+
+app.get('/getrawavalancheproof', async (req, res) => {
+  const proofid = req.query.proofid;
+
+  try {
+    const response = await axios.post(rpcURL, {
+      jsonrpc: '1.0',
+      id: 'curltest',
+      method: 'getrawavalancheproof',
+      params: [proofid]
+    }, {
+      auth: {
+        username: rpcUser,
+        password: rpcPassword
+      }
+    });
+
+    res.send(response.data.result);
+  } catch (error) {
+    handleRPCError(error, res);
+  }
+});
+
+app.get('/verifyavalancheproof', async (req, res) => {
+  const proof = req.query.proof;
+
+  try {
+    const response = await axios.post(rpcURL, {
+      jsonrpc: '1.0',
+      id: 'curltest',
+      method: 'verifyavalancheproof',
+      params: [proof]
+    }, {
+      auth: {
+        username: rpcUser,
+        password: rpcPassword
+      }
+    });
+
+    res.send(response.data.result);
+  } catch (error) {
+    handleRPCError(error, res);
+  }
+});
+
+
+
+function handleRPCError(error, res) {
+  if (error.response) {
+    res.status(500).send(error.response.data);
+  } else if (error.request) {
+    res.status(500).send('No response from RPC server');
+  } else {
+    res.status(500).send('Error setting up RPC request');
+  }
+}
+
+app.listen(port, () => {
+  console.log(`Server B API is running at http://localhost:${port}`);
+});
